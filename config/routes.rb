d4feafab79e42135d325aafbd3a306ba5152aaf8
lucie galichon <lgalichon@gmail.com>
2015-10-30 10:39:22 +0100

    last changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create, :edit] do
     resources :doses, only: [:index, :show, :new, :create, :edit, :destroy]
   end
